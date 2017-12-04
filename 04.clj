de93ddab20a07376464ba56b5922b56c36aaf772
Scott Anthony Murray <what.even.are.monads@gmail.com>
2017-12-04 00:50:52 -0500

    Added day 4

---
+++ 04.clj
@@ -0,0 +1,32 @@
+; https://adventofcode.com/2017/day/4
+
+(ns advent.four)
+(require '[clojure.string :as str])
+
+(defn passphrase-tokens
+  [passphrase]
+  (str/split passphrase #"\s"))
+
+(defn token-frequencies
+  [tokens]
+  (vals (frequencies tokens)))
+
+(defn all-frequencies-one?
+  [token-frequencies]
+  (every? #{1} token-frequencies))
+
+(defn is-valid-passphrase?
+  [passphrase]
+  (-> passphrase
+      (passphrase-tokens)
+      (token-frequencies)
+      (all-frequencies-one?)))
+
+(defn num-valid-passphrases
+  [passphrases]
+  (count (filter is-valid-passphrase? passphrases)))
+
+(def passphrases (-> (slurp "passphrases.txt")
+                     (str/split-lines)))
+
+(num-valid-passphrases passphrases) ; => 455
